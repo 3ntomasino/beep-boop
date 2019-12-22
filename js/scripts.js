0a23c8d3ab0afcd0da159b57dbcb5a78fcb0eab3
Ian Tomasino <usesomeink@gmail.com>
2019-12-21 18:01:26 -0800

    Start to add JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 $(document).ready(function()  {
-  $("form#userInput").submit(function(event)  {
+  $("form").submit(function(event)  {
     event.preventDefault();
-      
+
+    var num = parseInt($("input#number")).val();
+
+    function robotTalk(num) {
+      var stringOfNums = ('');
+      for (var i = 0; i <= num; i++) {
+        stringOfNums += i + (',');
+      }
+
+      var arrayOfNums = stringOfNums.split(',');
+
+      for (var i = 0; i < arrayOfNums.length; i++) {
+        var singleNum = arrayOfNums[i];
+
+        if (singleNum.includes(1) && singleNum.includes(2)) {
+          singleNum = "Boop";
+      } else if (singleNum.includes(1) && singleNum.includes(3)) {
+        singleNum = "I'm sorry, Dave. I'm afraid I can't do that.";
+      }
+
+      arrayOfNums.forEach(function(element) {
+        $("ul#result").append("<li>" + element + "</li>");
+      });
+
+    }
 
   });
 });
